c479bd36c961be58a5aa5c5dcccd5e50d295e637
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-06-02 16:54:50 +0100

    added PATCH / request handler taking an id parameter for employee.js

--- controllers/employee.js
+++ controllers/employee.js
@@ -61,9 +61,71 @@ const postEmployee = (request, response, next) => {
         }));
 };
 
+const updateEmployee = (request, response, next) => {
+    const id = request.params.id;
+    const allowedKeys = Object.keys(new Employee());
+    const entryKeys = Object.keys(request.body);    
+    const entryStates = entryKeys.map(key => allowedKeys.includes(key));
+    
+    if (entryStates.includes(false)) {
+        return response.status(400).json({
+            message: 'Unexpected request field(s)'
+        });
+    }
 
+    const email = request.body.email;
+    const dob = request.body.dob;
+    
+    if (entryKeys.includes('email') && !validateEmail(email)) {
+        return response.status(400).json({
+            message: 'Invalid email format'
+        });
+    }
+    
+    if (entryKeys.includes('dob') && !validateDate(dob)) {
+        return response.status(400).json({
+            message: 'Invalid dob (date of birth) format. Use the format yyyy-mm-dd'
+        });
+    }
+    
+    const entries = Object.entries(request.body).filter(el => !el.includes('id'));
+    let queryString = '';
+    const querySet = [];
+    entries.forEach((el, ind, arr) => {
+        if (ind < arr.length - 1) {
+            queryString += `${el[0]}=$${ind + 1}, `;
+        } else {
+            queryString += `${el[0]}=$${ind + 1}`;
+        }
+        querySet.push(el[1]);
+    });
+
+    if (querySet.length) {
+        querySet.push(id);
+        queryString = `UPDATE employee SET ${queryString} WHERE id=$${querySet.length};`;
+        dbConnection.dbConnect(queryString, querySet)
+            .then(res => {
+                if (res.command == 'UPDATE') {
+                    return dbConnection.dbConnect('SELECT * FROM employee WHERE id=$1;', [id]);
+                }
+                const error = new Error('Unable to update employee record');
+                throw error;
+            })
+            .then(res => response.status(200).json({
+                employee: res.rows[0]
+            }))
+            .catch(error => response.status(500).json({
+                message: error.message
+            }));
+    } else {
+        response.status(400).json({
+            message: 'Invalid request body'
+        });
+    }
+};
 
 module.exports = {
     getEmployees,
     postEmployee,
+    updateEmployee
 };
