bffb2db6ef3e640cfbf1e03a1c21a0dd86e17061
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-06-02 16:45:52 +0100

    added employee model

---
+++ models/employee.js
@@ -0,0 +1,10 @@
+module.exports = class Employee {
+    constructor() {
+        this.id = null;
+        this.first_name = null;
+        this.last_name = null;
+        this.role = null;
+        this.email = null;
+        this.dob = null;
+    }
+}
