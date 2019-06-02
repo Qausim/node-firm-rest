ec2ffe701799d36bf6678f59c272872e077359e3
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-06-02 16:53:53 +0100

    added POST / request handler for employee.js

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,6 +2,13 @@ const {dbConnection} = require('../config/database');
 require('../config/migration').createEmployeeTable();
 const Employee = require('../models/employee');
 
+const validateEmail = email => {
+    return /[-a-zA-Z0-9._]+@{1}[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)?\.[a-zA-Z]+/.test(email);
+};
+
+const validateDate = date => {
+    return /\d{4}\-(?:[0][1-9]|[1][0-2])\-(?:[0-2][1-9]|[3][01])/.test(date);
+};
 
 const getEmployees = (request, response, next) => {
     dbConnection.dbConnect('SELECT * FROM employee;')
@@ -13,8 +20,50 @@ const getEmployees = (request, response, next) => {
         }));
 };
 
+const postEmployee = (request, response, next) => {
+    
+    const email = request.body.email;
+    if (!validateEmail(email)) {
+        return response.status(400).json({
+            message: 'Invalid email format'
+        });
+    }
+    const dob = request.body.dob;
+    if (!validateDate(dob)) {
+        return response.status(400).json({
+            message: 'Invalid dob (date of birth) format. Use the format yyyy-mm-dd'
+        });
+    }
+    dbConnection.dbConnect(`INSERT INTO employee (first_name, last_name, role, email, dob) VALUES
+        ($1, $2, $3, $4, $5);`, [request.body.first_name, request.body.last_name,
+        request.body.role, email, dob])
+        .then(res => {
+            if (res.command != 'INSERT') {
+                return dbConnection.dbConnect('SELECT * FROM employee WHERE email=$1', [email]);   
+            }
+        })
+        .then(res => {
+            const selected = res.rows[0];
+            const employee = new Employee();
+            employee.id = selected.id;
+            employee.first_name = selected.first_name;
+            employee.last_name = selected.last_name;
+            employee.role = selected.role;
+            employee.email = selected.email;
+            employee.dob = selected.dob;
+                
+            return response.status(201).json({
+                employee
+            });
+        })
+        .catch(error => response.status(500).json({
+            message: error.message
+        }));
+};
+
 
 
 module.exports = {
     getEmployees,
+    postEmployee,
 };
